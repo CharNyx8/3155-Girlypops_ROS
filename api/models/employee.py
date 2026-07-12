8677698488714f31cc06ae86c847198c2263382d
Bindu Harini Gunturi <bgunturi@charlotte.edu>
2026-07-12 17:35:54 -0400

    feat: implement restaurant employee model and schemas

---
+++ api/models/employee.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from ..dependencies.database import Base
+
+
+class RestaurantEmployee(Base):
+    __tablename__ = "restaurant_employees"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    employee_id = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    role = Column(String, nullable=False)
